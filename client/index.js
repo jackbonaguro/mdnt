da7fe7ef2a53f0db0c5446488f79b6587eb8e1b2
Jack Bonaguro <jbonaguro@bitpay.com>
2020-07-09 20:34:02 -0400

    Unbreak next

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 const next = require('next');
 const dev = process.env.NODE_DEV !== 'production' //true false
-const nextApp = next({ dev });
+const nextApp = next({
+    dev,
+    dir: './client'
+});
 
 module.exports = nextApp;
