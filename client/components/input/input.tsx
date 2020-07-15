009b88d88db98e6f74921dc614d63e646da12ed0
Ash Bhimasani <abhimasani@bitpay.com>
2020-07-15 18:51:11 -0400

    add form and input styles and shared styles

---
+++ client/components/input/input.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './input.module.scss';
+
+const Input: React.FunctionComponent = () => {
+  return (
+    <div>wiggle</div>
+  );
+}
+  
+export default Input;
