c26b2cb82d1c7192778a25598861bfbed7006bf4
Ash Bhimasani <abhimasani@bitpay.com>
2020-07-24 15:03:57 -0400

    convert _app to typescript

--- client/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/styles.scss'
-
-// This default export is required in a new `pages/_app.js` file.
-export default function MidnightCash({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
---
+++ client/pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/styles.scss'
+
+const MidnightCash: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default MidnightCash;
